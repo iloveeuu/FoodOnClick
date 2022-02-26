34f49918ea6cda80440ed6828f26332d4108d588
Jerron <jerron.lim@hotmail.com>
2022-02-26 14:39:52 +0800

    Setup SFTP class, and example of how to use the EMAIL system

---
+++ FoodOnClick/SendMailExample.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="SendMailExample.aspx.vb" Inherits="FoodOnClick.SendMailExample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button runat="server" ID="btnSendMail" Text="Send Mail" OnClick="btnSendMail_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FoodOnClick/SendMailExample.aspx.vb
@@ -0,0 +1,24 @@
+﻿Public Class SendMailExample
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnSendMail_Click(sender As Object, e As EventArgs)
+        Dim mail As New SMTP()
+        'Apparently cannot send to same email twice, only one will receive.
+        Dim ToAddressies As String() = {"fyp-foodonclick@hotmail.com", "fypfoodonclick@gmail.com"}
+        Dim attachs() As String = {}
+        '"d:\temp_Excell226.xlsx", "d:\temp_Excell224.xlsx", "d:\temp_Excell225.xlsx"
+        'Attachment never try, not sure if we need it.
+        Dim subject As String = "Testing on local"
+        Dim body As String = "From local pc hello"
+        Dim result As Boolean = mail.SendMail(ToAddressies, subject, body, attachs)
+        If result Then
+            MsgBox("mails sended successfully", MsgBoxStyle.Information)
+        Else
+            MsgBox(mail.ErrorText, MsgBoxStyle.Critical)
+        End If
+    End Sub
+End Class
