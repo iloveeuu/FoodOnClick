726ddab9ab2b4312d09427c355f27d664f4874e8
Jerron <jerron.lim@hotmail.com>
2022-03-05 19:15:54 +0800

    Added login page that is successfully able to redirect

--- FoodOnClick/Class/User.vb
+++ FoodOnClick/Class/User.vb
@@ -1,8 +1,72 @@
-﻿Public Class User
+﻿Imports System.Data.SqlClient
 
+Public Class User
+
+    Protected str_firstName As String
+    Protected str_lastName As String
+    Protected str_address As String
+    Protected int_phone As Integer
+    Protected str_gender As String
+    Protected str_dateOfBirth As String
+    Protected str_type As String
     Protected str_username As String
     Protected str_password As String
-
+    Public Property firstName() As String
+        Get
+            firstName = str_firstName
+        End Get
+        Set(ByVal Value As String)
+            str_firstName = Value
+        End Set
+    End Property
+    Public Property lastName() As String
+        Get
+            lastName = str_lastName
+        End Get
+        Set(ByVal Value As String)
+            str_lastName = Value
+        End Set
+    End Property
+    Public Property address() As String
+        Get
+            address = str_address
+        End Get
+        Set(ByVal Value As String)
+            str_address = Value
+        End Set
+    End Property
+    Public Property phone() As Integer
+        Get
+            phone = int_phone
+        End Get
+        Set(ByVal Value As Integer)
+            int_phone = Value
+        End Set
+    End Property
+    Public Property gender() As String
+        Get
+            gender = str_gender
+        End Get
+        Set(ByVal Value As String)
+            str_gender = Value
+        End Set
+    End Property
+    Public Property dateOfBirth() As String
+        Get
+            dateOfBirth = str_dateOfBirth
+        End Get
+        Set(ByVal Value As String)
+            str_dateOfBirth = Value
+        End Set
+    End Property
+    Public Property type() As String
+        Get
+            type = str_type
+        End Get
+        Set(ByVal Value As String)
+            str_type = Value
+        End Set
+    End Property
     Public Property username() As String
         Get
             username = str_username
@@ -21,14 +85,57 @@
     End Property
 
 
+    Public Sub New(ByVal username As String, ByVal password As String)
+        str_username = username
+        str_password = password
+    End Sub
+
     Public Sub New()
         str_username = ""
         str_password = ""
     End Sub
 
-    Public Sub New(ByVal username As String, ByVal password As String)
+    Public Sub New(ByVal firstName As String, ByVal lastName As String, ByVal address As String, ByVal phone As Integer, ByVal gender As String, ByVal dateOfBirth As String, ByVal type As String, ByVal username As String, ByVal password As String)
+        str_firstName = firstName
+        str_lastName = lastName
+        str_address = address
+        int_phone = phone
+        str_gender = gender
+        str_dateOfBirth = dateOfBirth
+        str_type = type
         str_username = username
         str_password = password
     End Sub
+
+    Public Function CheckUserLoginAccess() As String
+        Dim returnMsg As String = ""
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+        Dim Query As String = "SELECT * from UserAccount where LOWER(username) = @username and password = @password"
+        Using conn As New SqlConnection(connectionString)
+
+            Using comm As New SqlCommand()
+                With comm
+                    Dim mycommand As SqlClient.SqlCommand = New SqlClient.SqlCommand()
+                    .Connection = conn
+                    .CommandType = CommandType.Text
+                    .CommandText = Query
+                    .Parameters.Add("@username", SqlDbType.NVarChar).Value = Me.str_username.ToLower()
+                    .Parameters.Add("@password", SqlDbType.NVarChar).Value = Me.str_password
+                End With
+                Try
+                    conn.Open()
+                    Dim reader As SqlDataReader = comm.ExecuteReader
+                    While reader.Read()
+                        returnMsg = reader("type")
+                        System.Web.HttpContext.Current.Session("userid") = reader("userId")
+                    End While
+
+                Catch ex As SqlException
+                    returnMsg = ex.Message
+                End Try
+            End Using
+        End Using
+        Return returnMsg
+    End Function
 
 End Class
---
+++ FoodOnClick/riderHome.aspx.vb
@@ -0,0 +1,22 @@
+﻿Public Class riderHome
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnNotifySupport_Click(sender As Object, e As EventArgs)
+        Dim mail As New SMTP()
+        Dim ToAddressies As String() = {"fypfoodonclick@gmail.com", "fypfoodonclick@gmail.com"}
+        Dim attachs() As String = {}
+        '"d:\temp_Excell226.xlsx", "d:\temp_Excell224.xlsx", "d:\temp_Excell225.xlsx"
+        Dim subject As String = "Testing on local"
+        Dim body As String = "From local pc hello"
+        Dim result As Boolean = mail.SendMail(ToAddressies, subject, body, attachs)
+        If result Then
+            MsgBox("mails sended successfully", MsgBoxStyle.Information)
+        Else
+            MsgBox(mail.ErrorText, MsgBoxStyle.Critical)
+        End If
+    End Sub
+End Class
