3dfb5c3594729d55aa168d011f5bbe9bd34498b6
iloveeuu <97896336+iloveeuu@users.noreply.github.com>
2022-03-14 20:13:38 +0800

    Updated to show firstname and lastname

--- FoodOnClick/Masterpage.Master.vb
+++ FoodOnClick/Masterpage.Master.vb
@@ -2,7 +2,11 @@
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        If (Not Session("userid") Is Nothing) Then
+            hlLoginSignUp.Text = "Welcome " & Session("firstname") & " " & Session("lastname")
+        Else
+            hlLoginSignUp.Text = "Login/Sign Up"
+        End If
     End Sub
 
 End Class
