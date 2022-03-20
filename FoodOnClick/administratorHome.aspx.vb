3298e8c2f1d82b562a229169d4e0702f1883ba24
树樑 盛 <shengshuliang@outlook.com>
2022-03-20 20:23:56 +0800

    Update Admin Home and Manage

---
+++ FoodOnClick/administratorHome.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class administratorHome
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub manageApprovalClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles manageApproval.Load
+        Response.Redirect("adminstratorManageRegistrations.aspx")
+    End Sub
+
+End Class
