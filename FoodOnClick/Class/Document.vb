adc2dc34fcb2eadd553c38dd68bd47a97ed1a124
iloveeuu <97896336+iloveeuu@users.noreply.github.com>
2022-03-29 18:06:18 +0800

    Editted for approve users

---
+++ FoodOnClick/Class/Document.vb
@@ -0,0 +1,106 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Document
+    Inherits User
+    Protected str_restaurantLogo As String
+    Protected str_businessLicense As String
+    Protected str_halal As String
+    Public Property restaurantLogo() As String
+        Get
+            restaurantLogo = str_restaurantLogo
+        End Get
+        Set(ByVal Value As String)
+            str_restaurantLogo = Value
+        End Set
+    End Property
+    Public Property businessLicense() As String
+        Get
+            businessLicense = str_businessLicense
+        End Get
+        Set(ByVal Value As String)
+            str_businessLicense = Value
+        End Set
+    End Property
+    Public Property halal() As String
+        Get
+            halal = str_halal
+        End Get
+        Set(ByVal Value As String)
+            str_halal = Value
+        End Set
+    End Property
+    Public Sub New()
+    End Sub
+    Public Sub New(ByVal userid As Int32)
+        MyBase.userId = userid
+    End Sub
+
+    Public Function GetRestaurantDocument()
+        Dim returnMsg As String = "False"
+        Dim returnObject As List(Of Document) = New List(Of Document)
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+        Dim Query As String = "SELECT document.type,url,document.userid from document join useraccount on document.userid = useraccount.userid where useraccount.status = 'VETTING' and useraccount.type = 'Restaurant'  order by document.userid asc"
+        Using conn As New SqlConnection(connectionString)
+
+            Using comm As New SqlCommand()
+                With comm
+                    Dim mycommand As SqlClient.SqlCommand = New SqlClient.SqlCommand()
+                    .Connection = conn
+                    .CommandType = CommandType.Text
+                    .CommandText = Query
+                End With
+                Try
+                    conn.Open()
+                    Dim reader As SqlDataReader = comm.ExecuteReader
+                    Dim counter As Integer = 1
+                    Dim tempobj As Document = New Document()
+                    While reader.Read()
+                        tempobj.userId = reader("userid")
+                        If (reader("type") = "Restaurant Logo") Then
+                            tempobj.restaurantLogo = reader("url")
+                        ElseIf (reader("type") = "Business License") Then
+                            tempobj.businessLicense = reader("url")
+                        ElseIf (reader("type") = "Halal") Then
+                            tempobj.halal = reader("url")
+                        End If
+                        If (counter Mod 3 = 0) Then
+                            returnObject.Add(tempobj)
+                            tempobj = New Document()
+                        End If
+                        counter += 1
+                    End While
+                    Dim tempobj1 As Document = New Document()
+                Catch ex As SqlException
+
+                End Try
+            End Using
+        End Using
+        Return returnObject
+    End Function
+    Public Function GetCountRestaurantUser()
+        Dim returnInt As Integer = 0
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+        Dim Query As String = "Select COUNT(DISTINCT(dbo.document.userid)) As COUNT FROM dbo.document join dbo.UserAccount On dbo.UserAccount.userid=dbo.document.userid where dbo.UserAccount.status='VETTING' and dbo.UserAccount.type = 'Restaurant'"
+        Using conn As New SqlConnection(connectionString)
+
+            Using comm As New SqlCommand()
+                With comm
+                    Dim mycommand As SqlClient.SqlCommand = New SqlClient.SqlCommand()
+                    .Connection = conn
+                    .CommandType = CommandType.Text
+                    .CommandText = Query
+                End With
+                Try
+                    conn.Open()
+                    Dim reader As SqlDataReader = comm.ExecuteReader
+                    While reader.Read()
+                        returnInt = Convert.ToInt32(reader("COUNT"))
+                    End While
+                Catch ex As SqlException
+
+                End Try
+            End Using
+        End Using
+        Return returnInt
+    End Function
+End Class
--- FoodOnClick/adminstratorManageRegistrations.aspx
+++ FoodOnClick/adminstratorManageRegistrations.aspx
@@ -1,52 +1,87 @@
 ﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Masterpage.Master" CodeBehind="adminstratorManageRegistrations.aspx.vb" Inherits="FoodOnClick.adminstratorManageRegistrations_aspxt" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-      <h1>Manage Approval</h1>
-    
-    <table>
-        <tr>
-            <td><h4>Total Pending Application</h4></td>
-            <td><asp:Label ID="totalPendingCase" runat="server"></asp:Label></td>
-        </tr>
-    </table>
-   
-    <br />
-    <br />
+    <h1>Manage Approval</h1>
 
     <table>
         <tr>
-            <td><h4>UserID</h4></td>
-            <td><asp:label ID="document_useriID" runat="server"></asp:label></td>
+            <td>
+                <h4>Total Pending Application</h4>
+            </td>
+            <td>
+                <asp:Label ID="totalPendingCase" runat="server"></asp:Label></td>
         </tr>
     </table>
-    
-   
+
+    <br />
+    <br />
+    <div style="overflow-x: auto; text-align: center; width: 100%">
+        <div style="width:40%;margin:0 auto">
+        <asp:Repeater runat="server" ID="rptUser" OnItemDataBound="rptUser_ItemDataBound" OnItemCommand="rptUser_ItemCommand" EnableViewState="false">
+            <ItemTemplate>
+                <div>
+                    <asp:Label runat="server" ID="lblUserId" Text='<%#Eval("userId") %>'></asp:Label>
+                    <asp:Label runat="server" ID="lblRestaurantLogo" Text=" Restaurant Logo :"></asp:Label>
+                    <asp:HyperLink runat="server" ID="restaurantLogo"></asp:HyperLink>
+                    <asp:Label runat="server" ID="lblBusinessLicense" Text="Business License :"></asp:Label>
+                    <asp:HyperLink runat="server" ID="businessLicense"></asp:HyperLink>
+                    <asp:Label runat="server" ID="lblHalal" Text="Halal :"></asp:Label>
+                    <asp:HyperLink runat="server" ID="halal"></asp:HyperLink>
+                    <asp:Button ID="systemAdminRegistration_Approve" Text="Approve" CommandName="Approve" runat="server" CommandArgument='<%#Eval("userId") %>'/>
+                    <asp:Button ID="systemAdminRegistration_Reject" Text="Reject" CommandName="Reject" runat="server" CommandArgument='<%#Eval("userId") %>'/>
+                </div>
+                <br />
+            </ItemTemplate>
+        </asp:Repeater>
+            </div>
+    </div>
+    <%--<table>
+        <tr>
+            <td>
+                <h4>UserID</h4>
+            </td>
+            <td>
+                <asp:Label ID="document_useriID" runat="server"></asp:Label></td>
+        </tr>
+    </table>
+
+
     <table>
         <tr>
-            <td><h2>Type</h2></td>        
-            <td><h2>URL</h2></td>
+            <td>
+                <h2>Type</h2>
+            </td>
+            <td>
+                <h2>URL</h2>
+            </td>
         </tr>
         <tr>
             <td>Restaurant Logo</td>
-            <td><asp:label ID="document_restaurantLogo" runat="server"></asp:label></td>
+            <td>
+                <asp:Label ID="document_restaurantLogo" runat="server"></asp:Label></td>
         </tr>
         <tr>
             <td>Business License</td>
-            <td><asp:label ID="document_businessLicense" runat="server"></asp:label></td>
+            <td>
+                <asp:Label ID="document_businessLicense" runat="server"></asp:Label></td>
         </tr>
         <tr>
             <td>Halal</td>
-            <td><asp:label ID="document_halal" runat="server"></asp:label></td>
+            <td>
+                <asp:Label ID="document_halal" runat="server"></asp:Label></td>
         </tr>
 
-    </table>
+    </table>--%>
 
-    <table>
-         <tr>
-            <td><asp:button id="systemAdminRegistration_Approve" Text="Approve" runat="server"/></td>
-            <td><asp:button id="systemAdminRegistration_Reject" Text="Reject" runat="server" /></td>
+<%--    <table>
+        <tr>
+            <td>
+                <asp:Button ID="systemAdminRegistration_Approve" Text="Approve" runat="server" /></td>
+            <td>
+                <asp:Button ID="systemAdminRegistration_Reject" Text="Reject" runat="server" /></td>
         </tr>
-    </table>
+    </table>--%>
 
 </asp:Content>
